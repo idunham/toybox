6110b82c1aac011c71f01b8bc5aeae40222c0c24
Isaac Dunham <idunham@lavabit.com>
2013-09-07 22:22:10 -0700

    Implement cpio -o (write archive).
    Includes some hammering on the old code,
    plus splitting it up to allow closing the archive.

--- toys/pending/cpio.c
+++ toys/pending/cpio.c
@@ -15,7 +15,7 @@ config CPIO
     -o  create archive (stdin is a list of files, stdout is an archive)
     -t  list files (stdin is an archive, stdout is a list of files)
     -H fmt   Write archive in specified format:
-    newc  new character format (default)
+    newc  SVR4 new character format (default)
 */
 #define FOR_cpio
 #include "toys.h"
@@ -27,7 +27,7 @@ char * fmt;
 /* Iterate through a list of files, read from stdin.
  * No users need rw.
  */
-void stdin_loopfiles(void (*function)(int fd, char *name))
+void loopfiles_stdin(void (*function)(int fd, char *name))
 {
   int fd;
   char *name = toybuf;
@@ -35,14 +35,16 @@ void stdin_loopfiles(void (*function)(int fd, char *name))
   while (name != NULL){
     memset(toybuf, 0, sizeof(toybuf));
     name = fgets(toybuf, sizeof(toybuf) - 1, stdin);
+    
     if (name != NULL) {
-      if (toybuf[strlen(name)] == '\n') { //if a line didn't fit, ignore.
-        toybuf[strlen(name)] = '\0';
+      if (toybuf[strlen(name) - 1] == '\n' ) { 
+        toybuf[strlen(name) - 1 ] = '\0';
 	fd = open(name, O_RDONLY);
 	if (fd > 0) {
           function(fd, name);
 	  close(fd);
 	}
+	errno = 0;
       }
     }
   }
@@ -54,7 +56,7 @@ struct cpio_newc_header {
   char    c_mode[8];
   char    c_uid[8];
   char    c_gid[8];
-  char    c_nlink[8]; //here
+  char    c_nlink[8];
   char    c_mtime[8];
   char    c_filesize[8];
   char    c_devmajor[8];
@@ -65,32 +67,54 @@ struct cpio_newc_header {
   char    c_check[8];
 };
 
-void write_cpio_member(int fd, char *name)
+void write_cpio_member(int fd, char *name, struct stat buf)
 {
-  size_t out = 0;
   struct cpio_newc_header *hdr;
-  hdr = malloc(sizeof(struct cpio_newc_header));
-  memset(&hdr, '0', sizeof(hdr));
+  size_t out = 0;
+  unsigned int n = 0x00000000, nlen = strlen(name);
+
+  hdr = malloc(sizeof(struct cpio_newc_header) + 1);
+  memset(hdr, '0', sizeof(struct cpio_newc_header));
+  snprintf((char *)(hdr), sizeof(struct cpio_newc_header)+1, 
+          "070701%08X%08X" "%08X%08X"
+	  "%08X%08X%08X"
+	   "%08X%08X" "%08X%08X%08X00000000",
+	   (unsigned int)(buf.st_ino), buf.st_mode, buf.st_uid, buf.st_gid, 
+	   buf.st_nlink, (uint32_t)(buf.st_mtime), (uint32_t)(buf.st_size), 
+	   major(buf.st_dev), minor(buf.st_dev),
+           major(buf.st_rdev), minor(buf.st_rdev), nlen+1);
+  write(1, hdr, sizeof(struct cpio_newc_header));
+  write(1, name, nlen);
+  write(1, &n, 4 - ((nlen + 2) % 4));
+  for (; (lseek(fd, 0, SEEK_CUR) < (uint32_t)(buf.st_size));) {
+    out = read(fd, toybuf, sizeof(toybuf));
+    if (out > 0) { 
+      write(1, toybuf, out);
+    }
+    if (errno || out < sizeof(toybuf)) break;
+  }
+  write(1, &n, 4 - (buf.st_size % 4));
+}
+
+void write_cpio_call(int fd, char *name)
+{
   struct stat buf;
   if (stat(name, &buf) == -1) return;
-  //strcpy(hdr.c_magic, "070701");
-  snprintf(&hdr, sizeof(hdr), "070701%08o%08o%08o%08o%08o"
-           "%08o%08o%08o%08o" "%08o%08o",
-	   buf.st_ino, buf.st_mode, buf.st_uid, buf.st_gid, buf.st_nlink,
-           buf.st_mtime, buf.st_size, major(buf.st_dev), minor(buf.st_dev),
-           major(buf.st_rdev), minor(buf.st_rdev), strlen(name) );
-  write(1, &hdr, sizeof(struct cpio_newc_header));
-  write(1, name, strlen(name));
-  while (out >= 0) {
-    out = read(fd, toybuf, sizeof(toybuf));
-    write(1, toybuf, out);
-  }
+  write_cpio_member(fd, name, buf);
 }
 
 void cpio_main(void)
 {
-  int fd;
-  fd = xopen("main.c", O_RDONLY);
-  write_cpio_member(fd, "main.c");
-  error_exit("Sorry, cpio isn't implemented yet");
+  switch (toys.optflags & (FLAG_i | FLAG_o | FLAG_t)) {
+    case FLAG_o:
+      loopfiles_stdin(write_cpio_call);
+      struct stat fake;
+      stat("/dev/null", &fake);
+      write_cpio_member(open("/dev/null", O_RDONLY), "TRAILER!!!", fake);
+      write(1, toybuf, 4096);
+
+    break;
+  default: 
+  error_exit("Bad mix of flags");
+  }
 }
